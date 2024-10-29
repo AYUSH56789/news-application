e1b04f1d62da447dd80a7e11deb001d7f28222a9
Ayush Singh <ayushsingh46026@gmal.com>
2024-10-29 18:07:48 +0530

    done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">News </Link>
+          <Link className="navbar-brand" to="/general">News </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
